986ea2d5dfd5cffe409bd88c74f4bd2ffca76570
Gurpreet <dhoat30@gmail.com>
2023-09-05 08:36:15 +1200

    added redirection

--- next.config.js
+++ next.config.js
@@ -9,18 +9,67 @@ const nextConfig = {
     env: {
         url: "https://data.greatspicetauranga.co.nz"
     },
-    // headers: () => {
-    //     if (process.env.NODE_ENV === 'development') {
-    //         return [
-    //             {
-    //                 source: '/_next/static/css/_app-client_src_app_globals_css.css',
-    //                 headers: [{ key: 'Vary', value: '*' }],
-    //             },
-    //         ];
-    //     } else {
-    //         return [];
-    //     }
-    // },
+    async redirects() {
+        return [
+            {
+                source: '/contact.html',
+                destination: '/contact',
+                permanent: true,
+            },
+            {
+                source: '/faq.html',
+                destination: '/faq',
+                permanent: true,
+            },
+            {
+                source: '/services.html',
+                destination: '/',
+                permanent: true,
+            },
+            {
+                source: '/reservation.html',
+                destination: '/reservation',
+                permanent: true,
+            },
+            {
+                source: '/takeaway-menu.html',
+                destination: '/menu/takeaway-menu',
+                permanent: true,
+            },
+            {
+                source: '/dine-in-menu.html',
+                destination: '/menu/dine-in-menu',
+                permanent: true,
+            },
+            {
+                source: '/news.html',
+                destination: '/blogs',
+                permanent: true,
+            },
+            {
+                source: '/gallery.html',
+                destination: '/gallery',
+                permanent: true,
+            },
+            {
+                source: '/menu/lunch-menu.pdf',
+                destination: '/menu/lunch-menu',
+                permanent: true,
+            },
+            {
+                source: '/dine.html',
+                destination: '/dine',
+                permanent: true,
+            },
+
+            {
+                source: '/news/:slug',
+                destination: '/blogs/:slug',
+                permanent: true,
+            },
+
+        ];
+    },
 
 }
 
